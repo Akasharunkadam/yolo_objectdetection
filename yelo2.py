01df3df67e0372afd560e596f7617fe0c973e218
Akash_Kadam <54442945+Akasharunkadam@users.noreply.github.com>
2021-02-11 13:11:19 +0530

    Add files via upload

---
+++ yelo2.py
@@ -0,0 +1,111 @@
+-opencvimport cv2
+import numpy as np
+import time
+import os
+# Load Yolo
+net = cv2.dnn.readNet("yolov2-tiny.weights", "yolov2-tiny.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Loading camera
+cap = cv2.VideoCapture(0)
+
+font = cv2.FONT_HERSHEY_PLAIN
+starting_time = time.time()
+frame_id = 0
+
+f=open("loger.txt","w+")
+
+while True:
+    _, frame = cap.read()
+    height, width, channels = frame.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(frame, 0.00492, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+    
+   
+
+    
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.2:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+                #print(center_x,center_y)
+                if(center_x<200):
+                    print("car is in left lane")
+                elif(center_x>400):
+                    print('car is in right lane')
+                else:
+                    print('car is in mid lane')
+
+                print(center_y)
+
+                if(center_y>320):
+                    print('car is close applay full brakes')
+                
+
+                
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.3)
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = confidences[i]
+            #print(indexes)
+
+            color = colors[class_ids[i]]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + 30), color, -3)
+            cv2.putText(frame, label + " " + str(round(confidence, 2)), (x, y + 30), font, 1, (255,255,255), 1)
+
+
+    elapsed_time = time.time() - starting_time
+    fps = frame_id / elapsed_time
+    #print(elapsed_time)
+    cv2.putText(frame, "FPS: " + str(round(fps, 2)), (10, 50), font, 1, (0, 0, 0), 1)
+    cv2.imshow("Image", frame)
+    vdf=0
+    for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                sub="person"
+                dele=classes.count(sub)
+                vdf=vdf+dele
+
+    f.write("COUNT OF PERSONS PRESENT IS %d\r\n" % vdf)
+    if(vdf>5):
+        print('there is traffic, controlling speed')
+
+    
+    key = cv2.waitKey(1)
+    if key == 32:
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
+
